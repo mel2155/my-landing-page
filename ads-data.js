2bb697259bad800b7fc38d6a45509ab525ab2d29
mel2155 <melyy179@gmail.com>
2025-07-15 17:40:42 +0800

    Update ads-data.js

--- ads-data.js
+++ ads-data.js
@@ -1,5 +1,4 @@
-// ads-data.js
-
+//ads-data.js
 
 const adsData = [
     {
@@ -10,13 +9,15 @@ const adsData = [
         link: "https://kyyl.im" 
     }
 
-
-   // ,{
+    /*
+    ,{
         name: "第二个广告",
         pcImage: "images/134498.gif",
         mobileImage: "images/43057.gif",
         alt: "第二个广告的描述",
         link: "https://example.com/second-ad-link"
     }
+    */
 
 ];
+
