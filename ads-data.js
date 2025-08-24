a2a80b03ff677359a1ee31aedd289dc3d4058121
mel2155 <melyy179@gmail.com>
2025-08-24 17:48:33 +0800

    Update ads-data.js

--- ads-data.js
+++ ads-data.js
@@ -9,13 +9,13 @@
         link: "https://m.uirates.com" 
     }
 
-    /*
+    
     ,{
         name: "第二个广告",
-        pcImage: "images/134498.gif",
-        mobileImage: "images/43057.gif",
+        pcImage: "https://img.251195.com/images/68aadc37658b3a4e0da29fa8.gif",
+        mobileImage: "https://img.251195.com/images/68aadc37658b3a4e0da29fa8.gif",
         alt: "第二个广告的描述",
-        link: "https://example.com/second-ad-link"
+        link: "https://ccc28859.top"
     }
     */
 
