491781e2164754db4490d8d4efffeeecfb165a08
xingquban <112711384+xingquban@users.noreply.github.com>
2025-10-22 14:06:18 +0700

    Update ads-data.js

--- ads-data.js
+++ ads-data.js
@@ -1,3 +1,21 @@
 //ads-data.js
+//const adsData = [
+    {
+        //name: "主要推广广告",
+        //pcImage: "images/ad-banner-pc.gif",          
+        //mobileImage: "images/ad-banner-mobile.gif",   
+        //alt: "haohaise.tv 的主要推广广告",
+        //link: "https://m.uirates.com" 
+    }
+
+    
+    ,{
+       // name: "第二个广告",
+       // pcImage: "https://img.251195.com/images/68aadc37658b3a4e0da29fa8.gif",
+       // mobileImage: "https://img.251195.com/images/68aadc37658b3a4e0da29fa8.gif",
+        //alt: "第二个广告的描述",
+        //link: "https://ccc28859.top"
+    }
+    
 ];
 
